74dcf9bb9b0820b24edfa0d1b5ec360d7f3081b4
Jerald Cascayan <casourcer@gmail.com>
2018-04-04 12:40:40 -1000

    refactored

--- IPmap.py
+++ IPmap.py
@@ -4,12 +4,9 @@ import socket
 print("convert to domain name's ipadd")
 print("example: google.com")
 
-
 try:
     while True:
-        
         data = raw_input("enter domain name : ")
-
         print(socket.gethostbyname(data))
 except KeyboardInterrupt:
     print("exiting")
